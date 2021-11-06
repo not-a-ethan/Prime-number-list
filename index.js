36b51f992a2a6e112fd67fbe3d36b5b3a72e754e
coolprojects <92686703+Ethan-Master-Coding@users.noreply.github.com>
2021-11-06 21:13:06 +0000

    Took it out of a function

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-function prime() {
-
 for (let i = 4; i = 9007199254740991; i++) {
   for (let j = 2; j >= i; j++) {
-    if (i % j){
+    if (i % j === 0){
     }else{
       console.log(i)
-      }
     }
   }
 }
