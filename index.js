0865424f07ba8569244de89b563a600688578716
coolprojects <92686703+Ethan-Master-Coding@users.noreply.github.com>
2021-11-12 20:39:55 +0000

    finished the project

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-for (let i = 4; i = 9007199254740991; i++) {
-  for (let j = 2; j >= i; j++) {
-    if (i % j === 0){
-    }else{
-      console.log(i)
+let composite = false
+
+for (let j = 2; j < 100000; j++) {
+  for (let i = 2; i < j; i++) {
+    if (j % i === 0 && j !== i){
+      composite = true
     }
+    
   }
+
+  if (composite === false) {
+   console.log(j)
+  }
+  composite = false
 }
